9e9cfa39625d521c95c52c00a25dc0956b37d3f8
Tom Jia <tomchristian1998@gmail.com>
2022-04-16 13:11:06 +0800

    added the routes for thoughts

--- routes/api/thoughts.js
+++ routes/api/thoughts.js
@@ -1,4 +1,99 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { User, Thought } = require("../../models");
 
+router.get("/", async (req, res) => {
+  //get all thoughts
+  try {
+    const getAllThoughts = await Thought.find();
+    res.status(200).json(getAllThoughts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:_id", async (req, res) => {
+  //get thought through id
+  try {
+    const getThought = await Thought.findOne({
+      _id: req.params._id,
+    });
+    res.status(200).json(getThought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/:userId", async (req, res) => {
+  //create new thought, and add to the user
+  try {
+    const createThought = await Thought.create(req.body);
+    const addThoughtToUser = await User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $push: { thoughts: createThought } },
+      {
+        new: true,
+      }
+    );
+    addThoughtToUser;
+    res.status(200).send(`thought added to user ${req.params.userId}`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:_id", async (req, res) => {
+  //update a thought through _id
+  try {
+    const updateThought = await Thought.findOneAndUpdate(
+      { _id: req.params._id },
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updateThought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:_id", async (req, res) => {
+  //delete thought through _id
+  try {
+    const deleteThought = await Thought.findOneAndDelete({
+      _id: req.params._id,
+    });
+    deleteThought;
+    res.status(200).send(`Thought deleted by id ${req.params._id}`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/:thoughtId/reactions", async (req, res) => {
+  //create a reaction associated with a thought
+  try {
+    const createReaction = await Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reaction: req.body } },
+      { new: true }
+    );
+    res.status(200).json(createReaction);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:thoughtId/reactions", async (req, res) => {
+  //delete a reaction associated with a thought
+  try {
+    const deleteReaction = await Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reaction: req.body } },
+      { new: true }
+    );
+    res.status(200).json(deleteReaction);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,76 +1,105 @@
-const router = require('express').Router();
-const { route } = require('.');
-const { User, Thought } = require('../../models');
+const router = require("express").Router();
+const { User, Thought } = require("../../models");
 
-router.get("/", async (req, res) =>{//get all users. 'get'-/api/users/
- try {
+router.get("/", async (req, res) => {
+  //get all users. 'get'-/api/users/
+  try {
     const getAllUsers = await User.find();
     res.status(200).json(getAllUsers);
-}
- catch (err) {
-    res.status(500).json(err)
- }
-});
-
-router.get('/:_id', async (req, res) => {//get user by id. 'get'-/api/users/:_id
- try {
-    const getUser = await User.findOne({
-        _id: req.params._id
-    }).populate('thoughts','friends')
-    res.status(200).json(getUser);
- }
- catch (err) {
-    res.status(500).json(err)
- }
-});
-
-router.post('/', async (req, res) => {//create user. 'post'-/api/users/
-    try {
-            User.create(req.body)
-            res.status(200).send('user created')
-
-        
-    }
-    catch (err) {
-        res.status(500).json(err)
-    }
-});
-
-
-router.put('/:_id', async (req, res) => {//update user info by _id. 'put'-/api/users/
-    try{
-        const updateUser = await User.findOneAndUpdate(
-            {'_id': req.params._id}, 
-             req.body, {
-                 new:true
-             }
-        );
-        res.status(200).json(updateUser) 
-     }
-    catch (err) {
-        res.status(500).json(err)
-    }
-});
-
-router.delete('/:_id', async (req, res) => {
-    try {
-            const findUsersThoughts = await User.findOne({_id: req.params._id})
-            const usersThoughts = findUsersThoughts.thoughts
-            usersThoughts.forEach(async element => {
-
-                const deleteThoughts =  await Thought.findOneAndRemove({
-                    '_id': element._id
-                })
-                deleteThoughts;
-            })
-        const deleteUser = await User.findOneAndRemove({
-        _id: req.params._id});
-        deleteUser;
-        res.status(200).send('user and their thoughts successfully deleted')
-} catch  (err) {
+  } catch (err) {
     res.status(500).json(err);
-}} )
+  }
+});
 
+router.get("/:_id", async (req, res) => {
+  //get user by id. 'get'-/api/users/:_id
+  try {
+    const getUser = await User.findOne({
+      _id: req.params._id,
+    }).populate("thoughts", "friends");
+    res.status(200).json(getUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-        
-    module.exports = router
+router.post("/", async (req, res) => {
+  //create user. 'post'-/api/users/
+  try {
+    const createUser = await User.create(req.body);
+    createUser;
+    res.status(200).send("user created");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:_id", async (req, res) => {
+  //update user info by _id. 'put'-/api/users/
+  try {
+    const updateUser = await User.findOneAndUpdate(
+      { _id: req.params._id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updateUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:_id", async (req, res) => {
+  //delete user and their thoughts
+  try {
+    const findUsersThoughts = await User.findOne({ _id: req.params._id });
+    if (findUsersThoughts.thoughts.length !== 0) {
+      const usersThoughts = findUsersThoughts.thoughts;
+      usersThoughts.forEach(async (element) => {
+        const deleteThoughts = await Thought.findOneAndRemove({
+          _id: element._id,
+        });
+        deleteThoughts;
+      });
+    }
+    const deleteUser = await User.findOneAndRemove({
+      _id: req.params._id,
+    });
+    deleteUser;
+    res.status(200).send("user and their thoughts successfully deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/:userId/friends/:friendId", async (req, res) => {
+  //add a friend
+  try {
+    const addFriend = await User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true }
+    );
+    res.status(200).json(addFriend);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:userId/friends/:friendId", async (req, res) => {
+  //remove a friend
+  try {
+    const addFriend = await User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    );
+    res.status(200).json(addFriend);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
